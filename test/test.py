b37fdbca65a3182f4b925c1b8469283556fdddc7
iron-island <154013605+iron-island@users.noreply.github.com>
2025-03-01 01:27:02 +0800

    test: add initial SPI write register test

--- test/test.py
+++ test/test.py
@@ -5,6 +5,27 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
+# SPI command constants
+WR_REG0_COMMAND = 0b00000
+WR_REG1_COMMAND = 0b00001
+WR_REG2_COMMAND = 0b00010
+
+def get_reg_bit(reg, bit):
+    # return reg[bit]
+    return (reg >> bit) & 1
+
+def update_bit(signal, bitval, bitstart, bitend=None):
+    '''
+    Wrapper function for updating bits/slices via a read-modify-write sequence
+    Ref: https://github.com/cocotb/cocotb/issues/4274#issuecomment-2537077592
+    '''
+    temp = signal
+    if (bitend == None):
+        temp[bitstart] = bitval
+    else:
+        temp[bitstart:bitend] = bitval
+
+    return temp
 
 @cocotb.test()
 async def test_project(dut):
@@ -24,14 +45,72 @@ async def test_project(dut):
     dut.ui_in.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 10)
 
-    dut._log.info("Test project behavior")
+    dut._log.info("SPI write test: WR_REG0_COMMAND")
 
-    # Set the input values you want to test
-    # TODO
+    #--------------------------#
+    # SPI Writing to Registers #
+    #--------------------------#
+
+    #spi_write(dut, WR_REG1_COMMAND, 0x0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f)
+    command = WR_REG1_COMMAND
+    data = 0xf0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0
+
+    # Drive SCK low and CSB low
+    dut.uio_in.value = 0b00000000
+
+    # Wait for 1 clock cycle delay
+    await ClockCycles(dut.clk, 1)
+    temp = dut.uio_in.value
+
+    # Drive command code
+    for bit in range(4, -1, -1):
+        # Drive MOSI
+        dut.uio_in.value = update_bit(temp, get_reg_bit(command, bit), 1)
+
+        # Wait for 1 clock cycle delay
+        await ClockCycles(dut.clk, 1)
+
+        # Drive SCK posedge
+        dut.uio_in.value = update_bit(dut.uio_in.value, 1, 3)
+
+        # Wait for 1 clock cycle delay
+        await ClockCycles(dut.clk, 1)
+
+        # Drive SCK negedge
+        temp = update_bit(dut.uio_in.value, 0, 3)
+
+    # Drive data
+    for bit in range(127, -1, -1):
+        # Drive MOSI
+        dut.uio_in.value = update_bit(temp, get_reg_bit(data, bit), 1)
+
+        # Wait for 1 clock cycle delay
+        await ClockCycles(dut.clk, 1)
+
+        # Drive SCK posedge
+        dut.uio_in.value = update_bit(dut.uio_in.value, 1, 3)
+
+        # Wait for 1 clock cycle delay
+        await ClockCycles(dut.clk, 1)
+
+        # Drive SCK negedge
+        temp = update_bit(dut.uio_in.value, 0, 3)
+
+    dut.uio_in.value = temp
+
+    # Wait for 1 clock cycle delay
+    await ClockCycles(dut.clk, 1)
+
+    # Drive CSB high
+    dut.uio_in.value = update_bit(dut.uio_in.value, 1, 0)
+
+    # Wait for 1 clock cycle delay
+    await ClockCycles(dut.clk, 36)
 
     # Wait for several clock cycles to see the output values
-    await ClockCycles(dut.clk, 36)
+    #await ClockCycles(dut.clk, 36)
 
     # TODO: add checkers
     # The following assersion is just an example of how to check the output values.
